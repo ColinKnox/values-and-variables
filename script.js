225e38811ee38635421311eb27c3d333332eb9fb
ColinKnox <colinwknox.com>
2024-11-02 20:28:02 -0400

    Adding starting code.

---
+++ script.js
@@ -0,0 +1,7 @@
+const district = "7th District";
+const city = "Boston";
+const usStateRepresentative = "Ayanna Pressley";
+
+console.log(district);
+console.log(city);
+console.log(usStateRepresentative);
